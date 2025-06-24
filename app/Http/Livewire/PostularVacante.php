67530ce49f0c37fef521ac5966874425e3f56580
Kevin <kevinsotomayor1@hotmail.com>
2025-06-24 18:28:07 -0500

    Primer commit

---
+++ app/Http/Livewire/PostularVacante.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Vacante;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class PostularVacante extends Component
+{
+    public $cv;
+    public $vacante;
+
+    use WithFileUploads;
+
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    public function mount(Vacante $vacante){
+        $this->vacante = $vacante;
+    }
+
+    public function postularme(){
+        $datos = $this->validate();
+
+        //Alamacenar cv en el disco duro
+        $cv = $this->cv->store('public/cv');
+        $datos['cv'] = str_replace('public/cv/', '', $cv);
+
+        //Crear el candidato a la vacante
+        $this->vacante->candidatos()->create([
+            'user_id' => auth()->user()->id,
+            'cv' => $datos['cv']
+        ]);
+
+        //Crear notificacion y enviar el email
+
+        //Mostrar el ususario un mensaje de ok
+        session()->flash('mensaje', 'Se envió correctamente tu información, mucha suerte');
+        return redirect()->back();
+    }
+
+    public function render()
+    {
+        return view('livewire.postular-vacante');
+    }
+}
